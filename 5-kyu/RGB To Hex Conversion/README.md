c2389328c572655032df39fea8c904917d4d7901
Urfeen <urfeens@gmail.com>
2021-06-18 14:42:29 +0300

    feat: add task description and solution - RGB to HEX conversion

--- 5-kyu/RGB To Hex Conversion/README.md
+++ 5-kyu/RGB To Hex Conversion/README.md
@@ -7,6 +7,5 @@ The following are examples of expected output values:
 ```javascript
 rgb(255, 255, 255) // returns FFFFFF
 rgb(255, 255, 300) // returns FFFFFF
-rgb(0,0,0) // returns 000000
 rgb(148, 0, 211) // returns 9400D3
 ```
--- 5-kyu/RGB To Hex Conversion/solution.js
+++ 5-kyu/RGB To Hex Conversion/solution.js
@@ -1,6 +1,6 @@
 function rgb(...rgb) {
   return rgb.map(num => num < 0 ? 0 : num > 255 ? 255 : num).reduce((acc, curr) => {
-    const hexCurr = curr.toString(16).toUpperCase();
-    return acc.concat(`${hexCurr.length === 1 ? '0' + hexCurr : hexCurr}`);
-  }, '');
+    const hexCurr = curr.toString(16).toUpperCase()
+    return acc.concat(`${hexCurr.length === 1 ? '0' + hexCurr : hexCurr}`)
+  }, '')
 }
